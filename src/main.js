ae17e576b766ab96cd1da42db7e4fa7b04ad7b5f
Edis <edis.sijaric95@gmail.com>
2021-05-10 17:18:28 +0200

    auth frontend

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import swal from 'vue-sweetalert2';
 
-Vue.config.productionTip = false
 
+Vue.use(swal);
+
+import "bootstrap/dist/css/bootstrap.css";
+
+const base = axios.create({
+  baseURL: "http://localhost:4000"
+});
+
+Vue.prototype.$http = base;
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
