5be82bdabb44449724d0414111b517831ca36bae
Edis <edis.sijaric95@gmail.com>
2021-05-12 01:51:17 +0200

    izlagaci dodani

--- src/components/products/AddProduct.vue
+++ src/components/products/AddProduct.vue
@@ -95,13 +95,84 @@
 
         <div class="form-group">
           <label for="exhibitor">Izlagač</label>
-          <input
+          <!-- <input
             class="form-control"
             id="exhibitor"
             required
             v-model="product.exhibitor"
             name="exhibitor"
-          />
+          /> -->
+          <select v-model="product.exhibitor">
+            <option disabled value="">Odaberi izlagača</option>
+            <option>MALAGIĆ NAMJEŠTAJ</option>
+            <option>DOMAVIJA NAMJEŠTAJ</option>
+            <option>WOOL CARPET</option>
+            <option>BOHA NAMJEŠTAJ</option>
+            <option>ARTI TEXTILE</option>
+            <option>Mobler 33</option>
+            <option>DIVAN NAMJESTAJ</option>
+            <option>ALESTRA</option>
+            <option>BOSNIAN BEECH BOAR</option>
+            <option>SUVENIRNICA MEVIĆ</option>
+            <option>REZBARIJE GOLETIĆ</option>
+            <option>FURNITURE LOVERS</option>
+            <option>ATRIKA HOME DESIGN</option>
+            <option>BERRE NAMJEŠTAJ</option>
+            <option>ID INTERIORS</option>
+            <option>R&S SARAJEVO</option>
+            <option>IMAN NAMJEŠTAJ PO MJERI</option>
+            <option>RATTAN SEDIA</option>
+            <option>MALAK JANJ</option>
+            <option>CFC ŽIVINICE</option>
+            <option>BREZJE NAMJEŠTAJ</option>
+            <option>SINMAX NAMJEŠTAJ</option>
+            <option>HRAST s.r. Kostajnica</option>
+            <option>MURGE s.r. Teslić</option>
+            <option>AG COMPANY Modriča</option>
+            <option>ČELEBIJA d.o.o. Sanski Most</option>
+            <option>PILANA PEPEX Zvornik</option>
+            <option>RAHIMIĆ DESIGN d.o.o. Prnjavor</option>
+            <option>MULIĆ s.r. Modriča</option>
+            <option>BAJRIĆ s.r. Maglaj</option>
+            <option>FINAL GROUP d.o.o. Sanski Most</option>
+            <option>ENI EM d.o.o. Doboj Jug</option>
+            <option>MARIN COMERCE d.o.o. Drvar</option>
+            <option>AHI TRADE d.o.o. Kalesija</option>
+            <option>ANJA I NAĐA d.o.o. Drvar</option>
+            <option>AD NAMJEŠTAJ z.r. Modriča</option>
+            <option>SANJO COM d.o.o.</option>
+            <option>IMEA WOOD Doboj</option>
+            <option>ASIKS d.o.o. Bosanski Petrovac</option>
+            <option>CVANIC d.o.o. Doboj</option>
+            <option>MASIV s.r. Doboj</option>
+            <option>M DIZAJN s.r. Teslić</option>
+            <option>DRVOELEGANT s.r Gornji Vakuf</option>
+            <option>DRVO-DIZAJN d.o.o. Odžak</option>
+            <option>IZAZOV d.o.o. Kalesija</option>
+            <option>PANEL HOLZ d.o.o. Prijedor</option>
+            <option>OBRT RAMIĆ Bosanski Petrovac</option>
+            <option>EURO BAU d.o.o. Ugljevik</option>
+            <option>BUDO EXPORT d.o.o. Žepče</option>
+            <option>SMARVET d.o.o. Ključ</option>
+            <option>SANI-GLOBAL Novi Grad</option>
+            <option>ZURAPOVIĆ DAMIR s.r. Šamac</option>
+            <option>ROSINJ d.o.o. Gornji Vakuf</option>
+            <option>EDO d.o.o. Kotor Varoš</option>
+            <option>Minalli</option>
+            <option>Alisa Teletovic</option>
+            <option>NEVA doo</option>
+            <option>agha-kids furniture</option>
+            <option>Mourah doo</option>
+            <option>Winner doo</option>
+            <option>Dallas International</option>
+            <option>G Wood by Goletić</option>
+            <option>Simperfect</option>
+            <option>WMTA</option>
+            <option>Mrak</option>
+            <option>Bapas Consulting</option>
+          </select>
+          <br />
+        <span> {{ product.exhibitor }}</span>
         </div>
 
         <br />
